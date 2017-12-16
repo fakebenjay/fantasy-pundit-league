a920c49e25c6cbe088152dbf8a6e089101f89b11
fakebenjay <fakebenjay@gmail.com>
2017-12-16 18:35:06 -0500

    hacky standings load fix, loads standings view on scroll

--- js/wrapper.js
+++ js/wrapper.js
@@ -1,11 +1,11 @@
-// function teamCall(name, week, quality, callback) {
-//   fetch(`https://fantasy-pundit-api.herokuapp.com/teams/${name}/${week}/${quality}`)
-//     .then(res => res.json())
-//     .then(json => callback(json))
-// }
-
 function teamCall(name, week, quality, callback) {
-  fetch(`http://localhost:3000/teams/${name}/${week}/${quality}`)
+  fetch(`https://fantasy-pundit-api.herokuapp.com/teams/${name}/${week}/${quality}`)
     .then(res => res.json())
     .then(json => callback(json))
 }
+
+// function teamCall(name, week, quality, callback) {
+//   fetch(`http://localhost:3000/teams/${name}/${week}/${quality}`)
+//     .then(res => res.json())
+//     .then(json => callback(json))
+// }
